8e5da82d0e95d3d0901a579bd8046c1d3e51ef37
Samuel Escobar <sescobar8714@gmail.com>
2020-02-16 22:36:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ original_array.each do | string |
 end
 new_array.join(" ")
 end
+
+def reverse_each_word_with_each(string)
+new_array = []
+original_array = string.split(" ")
+original_array.each do | string |
+new_array << string.reverse
+end
+new_array.join(" ")
+end
