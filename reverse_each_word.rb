2909ac3caf1cc5c46d845599625c830e5225849a
Samuel Escobar <sescobar8714@gmail.com>
2020-02-16 22:37:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,7 @@ new_array.join(" ")
 end
 
 def reverse_each_word_with_each(string)
-
+  
 new_array = []
 original_array = string.split(" ")
 original_array.each do | string |
@@ -16,3 +16,5 @@ new_array << string.reverse
 end
 new_array.join(" ")
 end
+
+reverse_each_word("Hello there, and how are you?")
